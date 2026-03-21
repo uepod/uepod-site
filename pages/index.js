a280ba76f61abae43217d7393deb7a7582e5ec64
uepod <hello@uepod.com>
2026-03-21 17:17:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -504,7 +504,7 @@ export default function Home({ episodes }) {
                     marginBottom: "40px",
                     color: "rgba(255,255,255,0.85)",
                   }}>
-                    Josh Stabinsky lives in San Francisco and spends a lot of time drinking coffee. He loves consumer packaged goods. And dogs.
+                    <a href="https://www.linkedin.com/in/stabinsky" target="_blank" rel="noopener noreferrer" style={{ color: "rgba(255,255,255,0.85)", textDecoration: "underline" }}>Josh Stabinsky</a> lives in San Francisco and spends a lot of time drinking coffee. He loves consumer packaged goods. And dogs.
                   </div>
 
                   <div style={{
